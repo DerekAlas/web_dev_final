4cc4b48556610c84e5b98e0573df33330f48563c
Derek Alas <daa9615@nyu.edu>
2022-12-12 19:33:48 -0500

    updated files

---
+++ script.js
@@ -0,0 +1,105 @@
+function openWindow(){
+  document.getElementById('infoWindow').style.display="grid";
+}
+
+function closeWindow(){
+  document.getElementById('infoWindow').style.display="none";
+}
+
+function openConvers(){
+  document.getElementById('conversWindow').style.display="grid";
+}
+
+function closeConvers(){
+  document.getElementById('conversWindow').style.display="none";
+}
+
+function openDogs(){
+  document.getElementById('dogsWindow').style.display="grid";
+}
+
+function closeDogs(){
+  document.getElementById('dogsWindow').style.display="none";
+}
+
+function openRear(){
+  document.getElementById('rearWindow').style.display="grid";
+}
+
+function closeRear(){
+  document.getElementById('rearWindow').style.display="none";
+}
+
+setTimeout(displayAd, 4900);
+
+function displayAd(){
+  document.getElementById('ad').style.display="grid";
+}
+
+function displayGame(){
+  document.getElementById('game-pop').style.display="grid";
+}
+
+function closeGame(){
+  document.getElementById('game-pop').style.display="none";
+}
+
+setTimeout(escape, 5000);
+
+function escape(){
+  document.getElementById('escape').style.display="grid";
+}
+
+setTimeout(smoke, 8000);
+
+function smoke(){
+  document.getElementById('smoke').style.display="grid";
+}
+
+setTimeout(horse, 10000);
+
+function horse(){
+  document.getElementById('horse').style.display="grid";
+}
+
+setTimeout(war, 12000);
+
+function war(){
+  document.getElementById('war').style.display="grid";
+}
+
+setTimeout(jewels, 13500);
+
+function jewels(){
+  document.getElementById('jewels').style.display="grid";
+}
+
+setTimeout(cocaine, 15000);
+
+function cocaine(){
+  document.getElementById('cocaine').style.display="grid";
+}
+
+setTimeout(mcd, 16000);
+
+function mcd(){
+  document.getElementById('mcd').style.display="grid";
+}
+
+setTimeout(dick, 17000);
+
+function dick(){
+  document.getElementById('dick').style.display="grid";
+}
+
+setTimeout(bkdick, 17500);
+
+function bkdick(){
+  document.getElementById('bkdick').style.display="grid";
+}
+
+setTimeout(streamad, 18000);
+
+function streamad(){
+  document.getElementById('streamad').style.display="grid";
+}
